3753fe71e308ca23c13089dcde2dd7021ac84044
Rodrigo Moreira <rodrigoalvesm06@gmail.com>
2021-10-03 18:27:50 -0300

    Initial Commit

---
+++ InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by Rodrigo Alves on 03/10/21.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
